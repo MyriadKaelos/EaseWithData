1e69f227697b1a8be9ebe4f620be0a98d02aaefd
MyriadKaelos <yateslough@students.berkeley.net>
2019-01-24 12:40:49 -0800

    Tried to Fix it, UGH

--- src/TestServer.java
+++ src/TestServer.java
@@ -22,18 +22,15 @@ public class TestServer extends Ease2 {
     public void send() {
         byteOut = new byte[]{(byte) (mouseX / 256), (byte) (mouseX % 256),
                 (byte) (ballX / 256), (byte) (ballX % 256),
-                (byte) (ballY / 256), (byte) (ballY % 256),
-                (byte) (vX / 256),(byte) (vX%256),
-                (byte) (vY / 256),(byte) (vY%256)};
+                (byte) ((height-ballY) / 256), (byte) ((height-ballY) % 256),
+                (byte) (vX+8),(byte) (8-vY)};
     }
     public void receive() {
         opponent = (int) byteIn[0] * 256 + ((int) byteIn[1] >= 0 ? ((int) byteIn[1]) : ((int) byteIn[1] + 256));
         ballX = (int) byteIn[2] * 256 + ((int) byteIn[3] >= 0 ? ((int) byteIn[3]) : ((int) byteIn[3] + 256));
         ballY = (int) byteIn[4] * 256 + ((int) byteIn[5] >= 0 ? ((int) byteIn[5]) : ((int) byteIn[5] + 256));
-        if((int) byteIn[6] * 256 + ((int) byteIn[7] >= 0 ? ((int) byteIn[7]) : ((int) byteIn[7] + 256)) != 0) {
-            vX = (int) byteIn[6] * 256 + ((int) byteIn[7] >= 0 ? ((int) byteIn[7]) : ((int) byteIn[7] + 256));
-            vY = (int) byteIn[8] * 256 + ((int) byteIn[9] >= 0 ? ((int) byteIn[9]) : ((int) byteIn[9] + 256));
-        }
+        vX = (int) byteIn[6] - 8;
+        vY = (int) byteIn[7] - 8;
     }
     public void ball() {
         if((ballX+(width/40) >= width && vX > 0) || (ballX <= 0 && vX < 0)) {
@@ -43,10 +40,9 @@ public class TestServer extends Ease2 {
                 (ballX+(width/40) > mouseX-(width/8) && ballX < mouseX+(width/8) && ballY < height/10 && ballY+(width/40) > height/20)) {
             vY *= -1;
         }
-        System.out.println(ballX);
+        System.out.println(ballX + " " + ballY);
         ballX += vX;
         ballY += vY;
-        System.out.println(ballX);
         rect(ballX,ballY,width/40,width/40,black);
     }
     public void me() {
